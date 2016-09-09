0f0a97a191d3b1d7446ca8ba9665796bea1207bd
pullconf <pullconf@jesusanswers.com>
2016-09-09 13:11:34 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,18 @@ DIR="$(pwd)"
 ## Update and install core packages
 apt-get update
 apt-get dist-upgrade -y
-apt-get install -y software-properties-common git curl wget
+apt-get install -y software-properties-common git curl wget gpg
+
+## Rng
+if [ -b /dev/hwrng ]; then
+    apt-get install -y rng-tools
+    cp -vf $DIR/etc/default/rng-tools /etc/default/rng-tools
+    systemctl enable rng-tools
+    systemctl restart rng-tools
+else
+    apt-get install -y haveged
+    cp -vf $DIR/etc/default/haveged /etc/default/haveged
+fi
 
 ## Install dnsmasq
 apt-get install -y dnsmasq
@@ -19,4 +30,17 @@ cp -vf $DIR/etc/apt/sources.list /etc/apt/sources.list
 apt-get update
 
 ## Setup Tor
-cp -vf $DIR/etc/apt/sources.list.d/xenial-torproject.list
+cp -vf $DIR/etc/apt/sources.list.d/xenial-deb.torproject.org.list /etc/apt/sources.list.d/xenial-deb.torproject.org.list
+gpg --keyserver keys.gnupg.net --recv 886DDD89
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+apt-get update
+apt-get install -y deb.torproject.org-keyring tor
+cp -vf $DIR/etc/tor/torrc /etc/tor/torrc
+
+## IPTables
+apt-get install iptables iptables-persistent
+cp -vf $DIR/etc/iptables/rules.v4 /etc/iptables/rules.v4
+cp -vf $DIR/etc/iptables/rules.v6 /etc/iptables/rules.v6
+iptables-restore < /etc/iptables/rules.v4
+ip6tables-restore < /etc/iptables/rules.v6
+
