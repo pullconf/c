577ebcd177a8b13e8ff94ee2b6d84f5e10c12121
pullconf <pullconf@jesusanswers.com>
2016-09-09 12:59:56 -0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Spin up script
+DIR="$(pwd)"
+
+## Update and install core packages
+apt-get update
+apt-get dist-upgrade -y
+apt-get install -y software-properties-common git curl wget
+
+## Install dnsmasq
+apt-get install -y dnsmasq
+cp -vf $DIR/etc/dnsmasq.conf /etc/dnsmasq.conf
