01b39287d7b61261f88949157befcf48a88ebd3e
pullconf <pullconf@jesusanswers.com>
2016-09-09 13:01:43 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,3 +10,10 @@ apt-get install -y software-properties-common git curl wget
 ## Install dnsmasq
 apt-get install -y dnsmasq
 cp -vf $DIR/etc/dnsmasq.conf /etc/dnsmasq.conf
+cp -vf $DIR/etc/default/dnsmasq /etc/default/dnsmasq
+systemctl enable dnsmasq
+systemctl restart dnsmasq
+
+## Setup APT
+cp -vf $DIR/etc/apt/sources.list /etc/apt/sources.list
+apt-get update
